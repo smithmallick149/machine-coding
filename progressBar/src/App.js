c57a478e183fcd48032ac14d2753eb1faf3ce1ef
smithmallick149 <smithmallick149@gmail.com>
2025-07-11 14:06:06 -0700

    progress bar

--- progressBar/src/App.js
+++ progressBar/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-import React from 'react';
+// import React from 'react';
+
+const ProgressBar = ({progress}) => {
+  return (
+    <div className='outer'>
+      <div className='inner' 
+        style={{
+          // width: `${progress}%`
+          transform: "translateX(${progress - 100}%)",
+        }} 
+        role='progressbar'
+        aria-valuenow={progress}
+        aria-valuemin="0"
+        aria-valuemax="100"
+        aria-label="Progress Bar"
+      >{progress}%</div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      data
+        <ProgressBar progress={90} />
     </div>
   );
 }
